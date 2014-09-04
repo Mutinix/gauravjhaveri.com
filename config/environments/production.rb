8ad240fd3509e96eefa22f20e3e5394a86186446
Gaurav Jhaveri <jhaveri.gaurav@gmail.com>
2014-09-04 23:16:42 +0530

    Fixes path of main.js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ Portfolio::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
-  config.assets.precompile += ["main.js"]
+  config.assets.precompile += ["bouncybox/main.js"]
 end

681009185b5e2ac5afa2b60194de439f1a354fb4
Gaurav Jhaveri <jhaveri.gaurav@gmail.com>
2014-09-30 01:04:37 +0530

    Updates configurations for Rails 4.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,9 +10,11 @@ Portfolio::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
+  
+  config.eager_load = true
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,2 @@
 Portfolio::Application.routes.draw do
-  root :to => 'high_voltage/pages#show', id: 'home'
 end
